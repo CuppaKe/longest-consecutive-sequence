d0ecb0d2a9c9950c317ba282117eb7c3de55c95b
CuppaKe <turnman87@gmail.com>
2018-10-06 12:29:11 +0300

    added function to count the longest length

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+/**
+ * Function finds the length of the longest consecutive elements sequence
+ * @param {array} array - unsorted array of integers
+ * @return {number}  longest length
+ */
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+  if (array.length === 0) return 0;
+  const mySet = new Set(array);
+  const iterator = mySet.values();
+  let start = iterator.next().value;
+  let maxCount = 1;
+  let tempCount = 1;
+  let i = 0;
+
+  while (i < mySet.size) {
+    if (mySet.has(start + 1)) {
+      tempCount++;
+      start += 1;
+      if (tempCount > maxCount) {
+        maxCount = tempCount;
+      }
+    } else {
+      i++;
+      tempCount = 1;
+      start = iterator.next().value;
+    }
+  }
+
+  return maxCount;
+};
